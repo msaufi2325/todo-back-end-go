fd26804e91c46b023b031d47319707d82c046021
msaufi2325 <msaufi2325@gmail.com>
2024-09-07 10:24:00 +0900

    Refactor Home handler to return JSON response

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to %s", app.Domain)
+	var payload = struct {
+		Status string `json:"status"`;
+		Message string `json:"message"`;
+		Version string `json:"version"`;
+	} {
+		Status: "active",
+		Message: "Todo API is up and running",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
