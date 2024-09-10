7ea195571b885d22f950d77bf3143a21778364f3
msaufi2325 <msaufi2325@gmail.com>
2024-09-10 20:41:33 +0900

    flag.StringVar read jwt related stuff

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/msaufi2325/todo-back-end-go/internal/repository"
 	"github.com/msaufi2325/todo-back-end-go/internal/repository/dbrepo"
 )
@@ -13,17 +16,40 @@ import (
 const port = 8081
 
 type application struct {
-	DSN string
-	Domain string
-	DB repository.DatabaseRepo
+	DSN          string
+	Domain       string
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
 }
 
 func main() {
+		// Load .env file
+		err := godotenv.Load("../../.env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+
 	// set application config
 	var app application
 
+	// Read JWT secret from environment variable
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+			fmt.Println("JWT_SECRET environment variable is not set")
+			return
+	}
+
 	// read from command line
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5434 user=postgres password=password dbname=todos sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", jwtSecret, "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "sigining issuer") // for development example.com is used
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
 	flag.Parse()
 
 	// connect to database
@@ -35,7 +61,16 @@ func main() {
 	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
 	defer app.DB.Connection().Close()
 
-	app.Domain = "example.com"
+	app.auth = Auth{
+		Issuer: app.JWTIssuer,
+		Audience: app.JWTAudience,
+		Secret: app.JWTSecret,
+		TokenExpiry: 15 * time.Minute,
+		RefreshExpiry: 7 * 24 * time.Hour,
+		CookiePath: "/",
+		CookieName: "__Host-refresh_token",
+		CookieDomain: app.CookieDomain,
+	}
 
 	log.Println("Starting server on", port)
 
