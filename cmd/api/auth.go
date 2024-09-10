97f026abe0f35bd39dff488a33cc235b16103f06
msaufi2325 <msaufi2325@gmail.com>
2024-09-10 19:42:55 +0900

    Refactor Auth.GenerateTokenPair to generate token pairs with signed access and refresh tokens

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -7,26 +7,73 @@ import (
 )
 
 type Auth struct {
-	Issuer      string
-	Audience    string
-	Secret      string
-	TokenExpiry time.Duration
+	Issuer        string
+	Audience      string
+	Secret        string
+	TokenExpiry   time.Duration
 	RefreshExpiry time.Duration
-	CookieDomain string
-	CookiePath string
-	CookieName string
+	CookieDomain  string
+	CookiePath    string
+	CookieName    string
 }
 
 type jwtUser struct {
-	ID int `json:"id"`
+	ID       int    `json:"id"`
 	Username string `json:"username"`
 }
 
 type TokenPairs struct {
-	Token string `json:"access_token"`
+	Token        string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 }
 
 type Claims struct {
 	jwt.RegisteredClaims
+}
+
+func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs, error) {
+	// create a token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set the claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = user.Username
+	claims["sub"] = user.ID // subject
+	claims["aud"] = j.Audience
+	claims["iss"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix() // issued at
+	claims["typ"] = "JWT"                   // type
+
+	// Set the expiry for JWT
+	claims["exp"] = time.Now().UTC().Add(j.TokenExpiry).Unix()
+
+	// Create a signed token
+	signedAccessToken, err := token.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// Create a refersh token and set claims
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	refreshTokenClaims := refreshToken.Claims.(jwt.MapClaims)
+	refreshTokenClaims["sub"] = user.ID                 // subject
+	refreshTokenClaims["iat"] = time.Now().UTC().Unix() // issued at
+
+	// Set the expiry for the refresh token
+	refreshTokenClaims["exp"] = time.Now().UTC().Add(j.RefreshExpiry).Unix()
+
+	// Create a signed refresh token
+	signedRefreshToken, err := refreshToken.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// Create TokenPairs and populate with signed tokens
+	var tokenPairs = TokenPairs{
+		Token:        signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+
+	// Return TokenPairs
+	return tokenPairs, nil
 }
