a4a62e575847c5c8fbf44704464dc6aa81884c38
msaufi2325 <msaufi2325@gmail.com>
2024-09-12 22:27:00 +0900

    Refactor handlers.go and routes.go to include logout functionality

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -127,3 +127,8 @@ func (app *application) refreshToken(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+
+func (app *application ) logout(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, app.auth.GetExpiredRefreshCookie())
+	app.writeJSON(w, http.StatusOK, "Logged out")
+}
