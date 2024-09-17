4e9fe00c4d30a54d1438405f78237c6b22150877
msaufi2325 <msaufi2325@gmail.com>
2024-09-18 00:22:26 +0900

    Update CORS settings to allow requests from the production domain

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,11 +4,10 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "https://todo-app.m-saufi.com")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Accept-Encoding, X-CSRF-Token, Authorization")
 			return
